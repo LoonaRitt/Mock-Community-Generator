0fa087f3f66a7d287e1b7f0cfd3bee8d155a0353
LoonaRitt <loona.rock@gmail.com>
2025-04-01 22:42:50 +0200

    setup_unix.sh

--- setup_unix.sh
+++ setup_unix.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Vérifier si Python est installé
-if ! command -v python &> /dev/null
+# Vérifier si Python3 est installé
+if ! command -v python3 &> /dev/null
 then
-    echo "Python n'est pas installé. Veuillez l'installer avant de continuer."
-    exit
+    echo "Python3 n'est pas installé. Veuillez l'installer avant de continuer."
+    exit 1
+fi
+
+# Vérifier si pip3 est installé
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 n'est pas installé. Installation en cours..."
+    sudo apt update && sudo apt install -y python3-pip
 fi
 
 # Installer les dépendances Python
 echo "Installation des dépendances Python..."
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Lancer le serveur Python
 echo "Lancement du serveur..."
-python server.py
+python3 server.py
